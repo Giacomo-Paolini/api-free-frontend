c449ad3a9390674dd7e2a9740f9536d576aeeee4
giacomopaolini1 <giacomopaolini08@gmail.com>
2023-11-28 18:04:55 +0100

    Update AppHeader styling and link font size

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,12 +1,12 @@
 <template>
-    <nav class="w-full grid grid-cols-3 gap-4 items-center p-4 pb-5">
-        <img src="/public/images/logo_transparent.png" class="max-w-[80px]" alt="logo.png">
+    <nav class="max-w-[1200px] mx-auto grid grid-cols-3 gap-4 items-center p-4 pb-5">
+        <img src="/public/images/logo_transparent.png" class="w-[120px]" alt="logo.png">
         <ul class="flex gap-x-3 justify-self-center">
             <li>
-                <a href="/" class="hover:underline hover:decoration-2">Home</a>
+                <a href="/" class="text-xl hover:underline hover:decoration-2">Home</a>
             </li>
             <li>
-                <a href="/new-article" class="hover:underline hover:decoration-2">New</a>
+                <a href="/new-article" class="text-xl hover:underline hover:decoration-2">New</a>
             </li>
         </ul>
     </nav>
