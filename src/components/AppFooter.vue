9aafa5ef29218236b3f60d7bbdc8cee80f2b9b7f
giacomopaolini1 <giacomopaolini08@gmail.com>
2023-11-28 17:56:32 +0100

    Update AppFooter styling

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex justify-center p-4 bg-white fixed inset-x-0 bottom-0 border-t-2 border-solid border-[--primary-color]">
+    <div class="flex justify-center p-2 bg-white fixed inset-x-0 bottom-0 border-t-2 border-solid border-[--primary-color]">
         <small>Made with <small class="text-[--primary-color]">&#10084;</small> by Giacomo</small>
     </div>
 </template>
