b9602803f5970289fdc8a18d4130865521b9cf59
giacomopaolini1 <giacomopaolini08@gmail.com>
2023-11-28 17:51:47 +0100

    Remove login and register links from AppHeader.vue

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -9,10 +9,6 @@
                 <a href="/new-article" class="hover:underline hover:decoration-2">New</a>
             </li>
         </ul>
-        <div class="flex gap-x-3 justify-self-end">
-            <a href="/" class="hover:underline hover:decoration-2">Login</a>
-            <a href="/" class="hover:underline hover:decoration-2">Register</a>
-        </div>
     </nav>
 </template>
 
